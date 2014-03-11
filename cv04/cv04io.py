01274c8b27f22be44945f5a1ae2da445c097fb19
Jozef Šiška <siska@ii.fmph.uniba.sk>
2014-03-12 00:42:21 +0100

    cv04/cv04io.py: ukazkovy program, ktory cita zadanie sudoku zo standard. vstupu

---
+++ cv04/cv04io.py
@@ -0,0 +1,43 @@
+#! /bin/env python
+#
+# Program, ktory nacita zadanie sudoku zo standartneho vstupu,
+# vyriesi ho (pomocou kniznice sudoku.py) a vypise riesenie
+# na standartny vystup.
+#
+# V linuxe ho mozete pustit napriklad:
+#
+# ./cv04io.py < sudoku.in
+#
+
+import sudoku
+import sys
+
+def die(msg):
+    sys.stderr.write('%s\n' % msg)
+    sys.exit(1)
+
+s = []
+try:
+    for line in sys.stdin:
+        if line.strip() != '':
+            row = [ int(x) for x in line.split() ]
+            if len(row) != 9:
+                raise ValueError("Wrong line on input")
+            s.append(row)
+    if len(s) != 9:
+        raise ValueError("Wrong number of lines")
+except ValueError as e:
+    die('Error reading input: %s' % (e,))
+
+
+try:
+    result = sudoku.SudokuSolver().solve(s)
+except e:
+    die('Error solving sudoku: %s' % (repr(e),))
+
+
+for row in result:
+    sys.stdout.write('%s\n' % ' '.join(map(str,row)))
+
+
+# vim: set sw=4 ts=4 sts=4 et :
